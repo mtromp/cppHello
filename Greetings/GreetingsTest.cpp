415f796e5b0cae9c8ada13905b813f6849680741
RJ Osborne <rjo1970@gmail.com>
2014-05-19 23:03:15 -0400

    Working in Ubuntu

---
+++ Greetings/GreetingsTest.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include "Greetings.h"
+
+TEST(GreetingsTest, WhenHelloIsCalledThenExpectHelloWorldString)
+{
+  Greetings* greetings = new Greetings();
+  EXPECT_EQ("Hello, world!", greetings->hello());
+  delete greetings;
+}
+
+int main(int argc, char* argv[])
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
